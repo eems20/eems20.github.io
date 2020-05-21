b039461a0eba2d4131a299276b8857bbc65a5aff
eems20 <55108010+eems20@users.noreply.github.com>
2020-05-21 10:44:56 -0700

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_POST['submit'])){
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['mail'];
+	$message = $_POST['message'];
+	
+	$mailTo = "erving.mar1@aol.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have receiven and email from ".$name.".\n\n".$message;
+	
+	
+	mail($mailTo,$subject, $txt, $headers );
+	header("Location: index.php?mailsend");
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+<head> 
+<meta charset="utf-8">
+<title>contact form </title>
+</head>
+<body>
+<main>
+<p>send email</p>
+<form class="contact-form" action = "contactform.php" method = "post">
+<input type="text" name="name" placeholder="Full name">
+<input type="text" name="mail" placeholder="your email">
+<input type="text" name="subject" placeholder="your subject">
+<textarea name="message" placeholder="Message"></textarea>
+<button type="submit" name="submit">send email</button>
+</form>
+</main>
+</body>
+
+</html>
